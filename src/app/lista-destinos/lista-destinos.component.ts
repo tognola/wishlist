e7ae5de726f13180457c0910b11929b3946219c3
Gastón Tognola <tognola08@gmail.com>
2019-12-30 10:10:51 -0300

    Primera app

---
+++ src/app/lista-destinos/lista-destinos.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-destinos',
+  templateUrl: './lista-destinos.component.html',
+  styleUrls: ['./lista-destinos.component.css']
+})
+export class ListaDestinosComponent implements OnInit {
+
+  destinos: string[];
+  constructor() {
+    this.destinos = ['Barcelona','Buenos Aires','Lima', 'Barranquilla'];
+  }
+
+  ngOnInit() {
+  }
+
+}
